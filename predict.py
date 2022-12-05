8c2d2f0465af43320d89de0aadde5d151ec18a94
ziqiang-sha <76215144+ziqiang-sha@users.noreply.github.com>
2022-12-05 12:43:58 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,6 +14,10 @@ import numpy as np
 from ecgdetectors import Detectors
 import os
 from typing import List, Tuple
+import numpy as np
+import torch
+import os
+from ecgdetectors import Detectors
 
 ###Signatur der Methode (Parameter und Anzahl return-Werte) darf nicht verändert werden
 def predict_labels(ecg_leads : List[np.ndarray], fs : float, ecg_names : List[str], model_name : str='model.npy',is_binary_classifier : bool=False) -> List[Tuple[str,str]]:
@@ -41,22 +45,55 @@ def predict_labels(ecg_leads : List[np.ndarray], fs : float, ecg_names : List[st
 
 #------------------------------------------------------------------------------
 # Euer Code ab hier  
-    with open(model_name, 'rb') as f:  
-        th_opt = np.load(f)         # Lade simples Model (1 Parameter)
 
-    detectors = Detectors(fs)        # Initialisierung des QRS-Detektors
+    FILE = 'model.pth'
+
+    # prediction model
+    def zero_pad(data, length):
+        extended = np.zeros(length)
+        siglength = np.min([length, len(data)])
+        extended[:siglength] = data[:siglength]
+        return extended
+
 
     predictions = list()
-    
-    for idx,ecg_lead in enumerate(ecg_leads):
-        r_peaks = detectors.hamilton_detector(ecg_lead)     # Detektion der QRS-Komplexe
-        sdnn = np.std(np.diff(r_peaks)/fs*1000) 
-        if sdnn < th_opt:
-            predictions.append((ecg_names[idx], 'N'))
-        else:
-            predictions.append((ecg_names[idx], 'A'))
-        if ((idx+1) % 100)==0:
+    res = []
+    data_name = []
+
+    for idx, ecg_lead in enumerate(ecg_leads):
+        mat_val = zero_pad(Detectors(300).hamilton_detector(mat_val),60)
+        data = mat_val - np.mean(mat_val) / np.std(mat_val)
+        # load the model
+        model = torch.load(FILE)    
+        model.eval()
+        output = model(data.float())
+        res.append(output.cpu().numpy().tolist())
+        data_name.append(ecg_names[idx])
+
+        if ((idx+1) % 100) == 0:
             print(str(idx+1) + "\t Dateien wurden verarbeitet.")
+
+    res = [i[0] for i in res]
+    print(f'res: {res}')
+    pred = torch.tensor(res)
+    print(f'pred: {pred}')
+    max_value, indices = pred.topk(1, dim=1, largest=True, sorted=True)
+    result = indices.tolist()
+    print(np.ravel(result).tolist())
+    result = np.ravel(result).tolist()
+
+    for i in result:
+        if i == 0:
+            predictions.append(('A'))
+        if i == 1:
+            predictions.append(('N'))
+        if i == 2:
+            predictions.append(('O'))
+        if i == 3:
+            predictions.append(('~'))
+    print(predictions)
+
+    predictions = list(zip(data_name, predictions))
             
             
 #------------------------------------------------------------------------------    
